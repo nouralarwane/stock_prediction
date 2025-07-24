3b495e085d22c1c8d85298f4243aec9197d56fd2
Larwanou Noura <larwanounoura49@gmail.com>
2025-07-24 02:10:30 +0100

    homepage react

---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+        <div className="container">
+            <div className="p-5 text-center bg-light-dark">
+                <h1 className='text-light'>Stock Prediction portal</h1>
+                <p className="text-light lead">
+                    This stock prediction application utilizes machine learning
+                    techniques, specifically employing Keras, and LSTM model,
+                    integrated within the Django Framework. It forecasts future
+                    stock prices by analyzing 100-day and 200-day moving averages, essential indicators
+                    widely used by stock analysts to inform trading and investment decisions.
+                </p>
+                <Button text="Login" class="btn-outline-info"/>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Main
